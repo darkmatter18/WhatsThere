badac8559cbd8156de6ed6aadc7e7431bf47ae14
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-05-22 17:13:32 +0530

    feat: Camera View added
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- app/src/main/java/com/arkadip/whatsthere/LogicActivity.java
+++ app/src/main/java/com/arkadip/whatsthere/LogicActivity.java
@@ -1,14 +1,125 @@
 package com.arkadip.whatsthere;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LogicActivity extends AppCompatActivity {
+
+    private PreviewView previewView;
+
+    private ExecutorService cameraExecutor;
+
+    private Camera camera;
+    private ProcessCameraProvider cameraProvider;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logic);
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        previewView = findViewById(R.id.preview_view);
+
+
+        previewView.post(this::setupCamera);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
+
+    private void setupCamera(){
+        cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderListenableFuture.addListener(() -> {
+            try {
+                cameraProvider = cameraProviderListenableFuture.get();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            bindCameraUsecases();
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindCameraUsecases() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        previewView.getDisplay().getRealMetrics(displayMetrics);
+        Log.d("DISPLAY", "Screen metrics: " + displayMetrics.widthPixels
+                + " x " + displayMetrics.heightPixels);
+
+        int aspectRatio = aspectRatio(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        Log.d("DISPLAY", "Preview aspect ratio: " + aspectRatio);
+
+        int rotation = previewView.getDisplay().getRotation();
+
+        //camera selector
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        //Preview
+        Preview preview = new Preview.Builder()
+                .setTargetAspectRatio(aspectRatio)
+                .setTargetRotation(rotation)
+                .build();
+
+        //Image Analysis
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetAspectRatio(aspectRatio)
+                .setTargetRotation(rotation)
+                .build();
+
+        imageAnalysis.setAnalyzer(cameraExecutor, image -> {
+
+            image.close();
+        });
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll();
+
+        camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis);
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
+    }
+
+    /**
+     * Detecting the most suitable ratio for dimensions provided in @params by counting absolute
+     * of preview ratio to one of the provided values.
+     *
+     * @param width  - preview width
+     * @param height - preview height
+     * @return suitable aspect ratio
+     */
+    private int aspectRatio(int width, int height) {
+        double RATIO_4_3_VALUE = 4.0 / 3.0;
+        double RATIO_16_9_VALUE = 16.0 / 9.0;
+
+        double previewRatio = (double) Math.max(width, height) / Math.min(width, height);
+        if (Math.abs(previewRatio - RATIO_4_3_VALUE) <= Math.abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3;
+        } else {
+            return AspectRatio.RATIO_16_9;
+        }
     }
 }
