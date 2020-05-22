5f65e17e9c09ac9063ff1b8c7e92f2396ab0f487
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-05-22 13:17:27 +0530

    feat: Added Permissions
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- app/src/main/java/com/arkadip/whatsthere/MainActivity.java
+++ app/src/main/java/com/arkadip/whatsthere/MainActivity.java
@@ -1,13 +1,24 @@
 package com.arkadip.whatsthere;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  String[] PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +30,40 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, LogicActivity.class);
-                startActivity(i);
-                finish();
+                if (!hasPermissions(MainActivity.this, PERMISSIONS)) {
+                    int PERMISSION_ALL = 1;
+                    ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, PERMISSION_ALL);
+                } else {
+                    openActivity();
+                }
             }
         }, 1000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("PERMISSION", "GIVEN");
+        openActivity();
+    }
+
+    private void openActivity(){
+        Intent i = new Intent(MainActivity.this, LogicActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        else{
+            return false;
+        }
+        return true;
     }
 }
