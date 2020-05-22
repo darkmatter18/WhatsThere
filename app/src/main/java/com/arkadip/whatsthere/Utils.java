c9ee09c9e407b93372fd02cbcd235ab32caf6063
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-05-22 17:56:33 +0530

    feat: Utils file added
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

---
+++ app/src/main/java/com/arkadip/whatsthere/Utils.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Arkadip Bhattacharya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.arkadip.whatsthere;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+class Utils {
+    static String assetFilePath(Context context, String assetName) {
+        File file = new File(context.getFilesDir(), assetName);
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return file.getAbsolutePath();
+        } catch (IOException e) {
+            Log.e("PYTORCH", "Error process asset " + assetName + " to file path");
+        }
+        return null;
+    }
+
+    static String loadJSONFromAsset(Context context, String JSONname){
+        String json;
+        try {
+            InputStream is = context.getAssets().open(JSONname);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+}
+
