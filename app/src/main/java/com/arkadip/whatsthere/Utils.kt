c6d28f64b7b404f23a8c4fd3b6f4beaf48c47dd3
lingarajsankaravelu <lingaraj.s@hotmail.com>
2020-10-11 21:28:14 +0530

    '#54'

--- app/src/main/java/com/arkadip/whatsthere/Utils.kt
+++ app/src/main/java/com/arkadip/whatsthere/Utils.kt
@@ -1,33 +1,14 @@
-/*
- * Copyright 2020 Arkadip Bhattacharya
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.arkadip.whatsthere
 
-package com.arkadip.whatsthere;
+import android.content.Context
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
-import android.content.Context;
-import android.util.Log;
+object Utils {
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-class Utils {
-
-    static String[] IMAGENET_CLASSES = new String[]{
+    var IMAGENET_CLASSES = arrayOf(
             "tench, Tinca tinca", "goldfish, Carassius auratus",
             "great white shark, white shark, man-eater, man-eating shark, Carcharodon carcharias",
             "tiger shark, Galeocerdo cuvieri", "hammerhead, hammerhead shark",
@@ -1001,27 +982,25 @@ class Utils {
             "bolete",
             "ear, spike, capitulum",
             "toilet tissue, toilet paper, bathroom tissue"
-    };
+    )
 
-    static String assetFilePath(Context context, String assetName) {
-        File file = new File(context.getFilesDir(), assetName);
-
-        try (InputStream is = context.getAssets().open(assetName)) {
-            try (OutputStream os = new FileOutputStream(file)) {
-                byte[] buffer = new byte[4 * 1024];
-                int read;
-                while ((read = is.read(buffer)) != -1) {
-                    os.write(buffer, 0, read);
-                }
-                os.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
+    fun  assetFilePath(context:Context, assetName:String):String? {
+        val file = File(context.filesDir, assetName)
+        try {
+            val inStream = context.assets.open(assetName)
+            val outStream = FileOutputStream(file)
+            val buffer = ByteArray(4 * 1024)
+            var read = 0
+            while (inStream.read(buffer).also { read = it }!=-1){
+                outStream.write(buffer,0,read)
             }
-            return file.getAbsolutePath();
-        } catch (IOException e) {
-            Log.e("PYTORCH", "Error process asset " + assetName + " to file path");
+            outStream.flush()
+            return  file.absolutePath
         }
-        return null;
+        catch (e:IOException) {
+            Log.e("PYTORCH", "Error process asset $assetName to file path")
+
+        }
+        return  null
     }
 }
-
